e917f80115dce91233a9df5568099b836c1435ad
wcmc-research-informatics <->
2019-07-03 13:14:25 -0400

    Minor edits

--- runrefresh.sh
+++ runrefresh.sh
@@ -1,6 +1,6 @@
 SHELL=/bin/bash
 # Example to run at 5 am each day: 
-# 00 5 * * * ~/boost/aourefresh/runrefresh.sh > /dev/null
+# 00 5 * * * /home/ras3005/boost/aourefresh/runrefresh.sh > /dev/null
 cd /home/ras3005/boost/aourefresh
 source /home/ras3005/boost/aourefresh/venv/bin/activate && python /home/ras3005/boost/aourefresh/refresh.py --site-config enclave/site-config.json --aou-api-spec enclave/aou-api-spec.json --db-spec enclave/p03.json
 
