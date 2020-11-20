8344b72ee00100e093a7bc0225553d7b81728a8f
wcmc-research-informatics <->
2020-11-20 17:34:28 -0500

    Update apitest

--- apitest/apitest.py
+++ apitest/apitest.py
@@ -1,7 +1,6 @@
 from google.oauth2 import service_account
 from google.auth.transport.requests import AuthorizedSession
 
-
 # pip install -r requirements.txt 
 
 # Set path to your key.json
@@ -14,9 +13,9 @@ authed_session = AuthorizedSession(scoped_credentials)
 # Set URL as appropriate for your site.
 response = authed_session.get('https://all-of-us-rdr-prod.appspot.com/rdr/v1/ParticipantSummary?_count=10&_sort=lastModified&awardee=COLUMBIA')
 
-print response.request.headers
-print response.request.body
-print response.status_code
-print response.headers
-print response.text
+print(response.request.headers)
+print(response.request.body)
+print(response.status_code)
+print(response.headers)
+print(response.text)
 
