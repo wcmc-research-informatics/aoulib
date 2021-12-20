2cc2bf9f7eed836fab547fa72623b8381192603d
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-20 10:34:07 +0800

    Delete runkeycycle.sh

--- runkeycycle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Example to run at 4 am each day: 
-# 00 4 * * * /home/ras3005/boost/aourefresh/runkeycycle.sh > /dev/null
-cd "$(dirname "${BASH_SOURCE[0]}")"
-source ./venv/bin/activate && python ./keycycle.py --site-config enclave/site-config.json --aou-api-spec enclave/aou-api-spec.json
-
