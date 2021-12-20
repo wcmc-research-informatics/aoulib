9fe4dce5a271b04d424726d4ec36d99661749fbf
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-20 10:30:24 +0800

    Delete installdeps.sh

--- installdeps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -z "${VIRTUAL_ENV+x}" ]; then
-  echo "need to be inside a virtualenv"
-  exit 1
-else 
-  echo "virtualenv folder is: $VIRTUAL_ENV"
-fi
-pip install -r requirements.txt --upgrade --upgrade-strategy eager
-
